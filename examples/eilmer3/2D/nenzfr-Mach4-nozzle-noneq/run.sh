4a1f73faff0fe3fa39d9df5c9871d5121276dc55
Peter Jacobs <peterj@mech.uq.edu.au>
2012-03-03 22:29:37 +1000

    nenzfr: scripts and tests in examples updated to specify nozzle options.
    Previously, I had had the Mach 4 nozzle as default in nenzfr and so
    didn't specify the nozzle in the test cases based on the Mach 4 nozzle.
    Recently, Luke has updated nenzfr to have the Mach 10 nozzle
    be the default; now these tests have to explicitly ask for the Mach 4 nozzle.

--- examples/eilmer3/2D/nenzfr-Mach4-nozzle-noneq/run.sh
+++ examples/eilmer3/2D/nenzfr-Mach4-nozzle-noneq/run.sh
@@ -2,5 +2,6 @@
 # run.sh
 
 echo "Start time: "; date
-nenzfr.py --gas=air5species --chem=neq --T1=300 --p1=125.0e3 --Vs=2414 --pe=34.376e6 --nni=600 --nnj=40 --by=1.02 --nbi=60 --max-time=1.0e-3 > LOGFILE
+nenzfr.py --gas=air5species --chem=neq --T1=300 --p1=125.0e3 --Vs=2414 --pe=34.376e6 --area=27.0 --cfile=contour-t4-m4.data \
+          --nni=600 --nnj=40 --by=1.02 --nbi=60 --max-time=1.0e-3 > LOGFILE
 echo "Finish time: "; date
