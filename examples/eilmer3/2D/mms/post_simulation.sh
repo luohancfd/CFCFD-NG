e96fcd5c75488c7727e6d7630428b0b0d3f02f36
Peter Jacobs <peterj@mech.uq.edu.au>
2012-03-18 19:16:16 +1000

    eilmer3 mms example: a little more clean-up of post-processing script.

--- examples/eilmer3/2D/mms/post_simulation.sh
+++ examples/eilmer3/2D/mms/post_simulation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 echo "Check that simulation has converged by comparing solution instances:"
-e3post.py --job=mms --tindx=6 \
+e3post.py --job=mms --tindx=6 --gmodel-file="very-viscous-air.lua" \
     --compare-job=mms --compare-tindx=20
-e3post.py --job=mms --tindx=7 \
+e3post.py --job=mms --tindx=7 --gmodel-file="very-viscous-air.lua" \
     --compare-job=mms --compare-tindx=20
 
 echo "----------------------------------------------------------------------"
 echo "Check simulation against analytical data:"
-e3post.py --job=mms --tindx=20 \
+e3post.py --job=mms --tindx=20 --gmodel-file="very-viscous-air.lua" \
     --ref-function=analytic_solution_wrapper.py \
     --per-block-norm-list="0,rho,L2;0,rho,L1" \
     --global-norm-list="rho,L2"
 
 echo "----------------------------------------------------------------------"
 echo "Generate VTK files for plotting:"
-e3post.py --job=mms --tindx=20 --vtk-xml
+e3post.py --job=mms --tindx=20 --gmodel-file="very-viscous-air.lua" \
+    --vtk-xml
 
