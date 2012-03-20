cbd892bcb9bc25aae108c56a1f35cfa15f736cf8
Peter Jacobs <peterj@mech.uq.edu.au>
2012-03-20 22:42:57 +1000

    eilmer3 Manufactured Solution viscous example made parallel.
    We now use a SuperBlock2D and the e3mpi executable.

--- examples/eilmer3/2D/mms/mms.py
+++ examples/eilmer3/2D/mms/mms.py
@@ -32,20 +32,10 @@ b = Node(1.0, 0.0, label="b")
 c = Node(0.0, 1.0, label="c")
 d = Node(1.0, 1.0, label="d")
 
-ab = Line(a, b)
-ac = Line(a, c)
-cd = Line(c, d)
-bd = Line(b, d)
-
-nx = 16
-ny = 16
-
-blk_0 = Block2D(make_patch(cd, bd, ab, ac), nni=nx, nnj=ny,
-                fill_condition=initial, label="blk-0")
-blk_0.set_BC(NORTH, USER_DEFINED, filename="udf-bc.lua")
-blk_0.set_BC(EAST, USER_DEFINED, filename="udf-bc.lua")
-blk_0.set_BC(SOUTH, USER_DEFINED, filename="udf-bc.lua")
-blk_0.set_BC(WEST, USER_DEFINED, filename="udf-bc.lua")
+blk = SuperBlock2D(make_patch(Line(c,d), Line(b,d), Line(a,b), Line(a,c)), 
+                   nni=16, nnj=16, nbi=2, nbj=2,
+                   bc_list=[UserDefinedBC("udf-bc.lua"),]*4,
+                   fill_condition=initial, label="blk")
 
 gdata.udf_file = "udf-source.lua"
 gdata.udf_source_vector_flag = 1
@@ -56,7 +46,7 @@ if case == 1:
     gdata.dt = 1.0e-6
     gdata.cfl = 0.5
 elif case == 2:
-    gdata.max_time = 200.0e-3
+    gdata.max_time = 80.0e-3
     gdata.max_step = 20000
     gdata.dt = 1.0e-6
     gdata.cfl = 0.5
