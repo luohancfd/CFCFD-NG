12516ac6753529f1a6c54cf6d2e0f315a3b608e0
Wilson Chan <y.chan1@uq.edu.au>
2012-04-27 11:56:13 +1000

    nenzfr: Updated comments and fixed bug in run-mpi-block-marching-mode.sh

--- examples/eilmer3/2D/nenzfr-Mach4-nozzle-eq/run-mpi-block-marching-mode.sh
+++ examples/eilmer3/2D/nenzfr-Mach4-nozzle-eq/run-mpi-block-marching-mode.sh
@@ -1,12 +1,11 @@
 #! /bin/bash 
 # run-mpi-block-marching-mode.sh
 #
-# The new multi-process block-marching mode that has recently 
-# been written into nenzfr.py. When running in this mode, ensure 
-# that you have the nozzle.timing file that specifies the max_time 
-# for each Eilmer3 run. 
+# Running nenzfr using the new multi-process block-marching mode 
+# that has recently been implemented. Note the two new options
+# needed when running in this mode --nbj and --block-marching.
 
 echo "Start time: "; date
 nenzfr.py --gas=air --T1=300 --p1=125.0e3 --Vs=2414 --pe=34.376e6 --area=27.0 --cfile=contour-t4-m4.data \
-          --nni=600 --nnj=40 --by=1.02 --nbi=60 --nbj=2 --block-marching > LOGFILE
+          --nni=600 --nnj=40 --by=1.02 --nbi=60 --nbj=2 --block-marching --max-time=1.0e-3 > LOGFILE
 echo "Finish time: "; date
