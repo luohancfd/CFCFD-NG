bb73ace98474b2645437e1cf5525ace329f12f5c
Rowan J. Gollan <r.gollan@uq.edu.au>
2012-10-26 22:08:15 +1000

    lib/gas: Refactored code to create gas filed; much tidier now.
    If Ken Thompson has
      "one of my most productive days was throwing away 1000 lines of code",
    then I'm pretty happy about removing 6 entire files.
    
    In this commit, the gasfile script to create gas model input files
    from a short textual description has been refactored to collect
    common code in one place. I was able to drop 6 files which duplicated
    a lot of code. The code now looks DRYer (Don't Repeat Yourself principle).
    
    I've left Dan's scripts as they were. A job for another day when I
    understand the details of what's occurring in that part of the code.

--- lib/gas/tools/two_temperature_gas.lua
+++ lib/gas/tools/two_temperature_gas.lua
@@ -4,35 +4,8 @@
 
 module(..., package.seeall)
 
------
--- Adapted from Listing 12.2 in
--- Programming in Lua, 2nd edition
------
-function serialise(o, f, indent)
-   indent = indent or ""
-   if type(o) == "number" then
-      f:write(o)
-   elseif type(o) == "string" then
-      f:write(string.format("%q", o))
-   elseif type(o) == "table" then
-      local old_indent = indent
-      indent = indent.."  "
-      f:write("{\n")
-      for k,v in pairs(o) do
-	 if type(k) == "string" then
-	    f:write(string.format("%s%s = ", indent, k))
-	 else
-	    f:write(string.format("%s", indent))
-	 end
-	 serialise(v, f, indent)
-	 f:write(",\n")
-      end
-      indent = old_indent
-      f:write(string.format("%s}", indent))
-   else
-      error("cannot serialise a " .. type(o))
-   end
-end
+require 'tab_serialise'
+local serialise = tab_serialise.serialise
 
 function list_available_species()
    e3bin = os.getenv("E3BIN") or os.getenv("HOME").."/e3bin"
