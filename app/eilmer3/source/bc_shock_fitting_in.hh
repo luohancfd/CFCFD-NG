1d59dfd7f07cad59f5f67fa5e81b82923ad3edd4
Andrew Pastrello <uqapastr@uq.edu.au>
2013-01-10 15:11:22 +1000

    Eilmer3: Missed some tabbing.

--- app/eilmer3/source/bc_shock_fitting_in.hh
+++ app/eilmer3/source/bc_shock_fitting_in.hh
@@ -13,8 +13,8 @@ public:
     // default apply_viscous() (does nothing)
 private:
     int calculate_shock_speed(FV_Cell *cL1, FV_Cell *cL0, FV_Cell *cR0, FV_Cell *cR1, FV_Cell *cR2, 
-				           double lenL1, double lenL0, double lenR0, double lenR1, double lenR2, 
-				           const FV_Interface *qL, FV_Interface *qR);
+			      double lenL1, double lenL0, double lenR0, double lenR1, double lenR2, 
+			      const FV_Interface *qL, FV_Interface *qR);
     int set_inflow_fluxes(FV_Interface *IFaceL, FV_Interface *IFaceR, double omegaz);
     inline double velocity_weighting_factor(double M);
 };
