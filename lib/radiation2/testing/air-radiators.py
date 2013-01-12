37fbe5b90b94b023b621c48abda4c107390847cd
Daniel Potter (laptop) <daniel.potter@uqconnect.edu.au>
2013-01-12 15:30:26 +1000

    lib/radiation2: work on the user guide

--- lib/radiation2/testing/air-radiators.py
+++ lib/radiation2/testing/air-radiators.py
@@ -1,38 +1,22 @@
-# A test input.py file for defining a radiation model
+# air-radiators.py: create a basic radiation model for 11 species 2 temperature air
 
-# first the spectral data
+# 1. Select the spectral model
 gdata.spectral_model = "photaura"
+
+# 2. Define the spectral grid
 gdata.lambda_min = 50.0
 gdata.lambda_max = 1000.0
 gdata.spectral_points = 95000
 
-# now transport model data
-gdata.transport_model = "optically thin"
-gdata.spectrally_resolved = True
-
-# now the radiators
-# dictionary of indices
-
-species_isps = {      "N2":  0,
-                 "N2_plus":  1,
-                      "NO":  2,
-                 "NO_plus":  3,
-                      "O2":  4,
-                 "O2_plus":  5,
-                       "N":  6,
-                  "N_plus":  7,
-                       "O":  8,
-                  "O_plus":  9,
-                 "e_minus": 10  }
-
-radiators = [ "N2", "N2_plus", "NO", "O2", "N", "N_plus", "O", "O_plus", "e_minus" ]
-
+# 3. Request and define the radiating species
+species   = [ "N2", "N2_plus", "NO", "NO_plus", "O2", "O2_plus",
+               "N",  "N_plus",  "O",  "O_plus", "e_minus" ]
+radiators = [ "N2", "N2_plus", "NO", "O2", "N", "N_plus",
+              "O", "O_plus", "e_minus" ]
 for rad_name in radiators:
     rad = gdata.request_radiator(rad_name)
-    rad.isp = species_isps[rad_name]
-    rad.iTe = 1
     rad.default_data()
+    rad.isp = species.index(rad_name)
+    rad.iTe = 1
     if rad.type == "diatomic_radiator":
         rad.iTv = 1
-
-# thats all we need to do!
