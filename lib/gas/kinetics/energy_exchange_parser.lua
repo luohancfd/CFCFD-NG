cf458a16a4da91a557c6646f403ecab6b8c3f6ac
Rowan J. Gollan <r.gollan@uq.edu.au>
2013-01-21 11:26:43 +1000

    lib/gas/kinetics: Energy exchange parser sets defaults for 'scheme' and 'ode' settings.
    The energy exchange parser now fills in some defaults for the
    'scheme' selection and 'ode' selection. This means that the input
    files appear less cluttered. In the examples section, the 'scheme'
    and 'ode' tables have been removed for the input for the Thivet
    example.

--- lib/gas/kinetics/energy_exchange_parser.lua
+++ lib/gas/kinetics/energy_exchange_parser.lua
@@ -9,8 +9,23 @@ local parse_energy_exch_string = exch.parse_energy_exch_string
 local validate_mechanism = exch.validate_mechanism
 local transform_mechanism = exch.transform_mechanism
 
-scheme_t = {}
-ode_t = {}
+-- Set defaults for scheme and ode solver
+scheme_t = {
+   update = "energy exchange ODE",
+   temperature_limits = {
+      lower = 20.0,
+      upper = 100000.0
+   },
+   error_tolerance = 0.000001
+}
+ode_t = {
+   step_routine = 'rkf',
+   max_step_attempts = 4,
+   max_increase_factor = 1.15,
+   max_decrease_factor = 0.01,
+   decrease_factor = 0.333
+}
+-- Empty table to gather mechanisms
 mechanisms = {}
 
 ---------------------------------------------
