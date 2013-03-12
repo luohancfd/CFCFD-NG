ac29a901e83cfe4e74357548a40edccfd2374d61
Peter Jacobs <peterj@mech.uq.edu.au>
2013-03-12 13:14:52 +0000

    gasmodel.py: Updated the documentation/help.
    Prompted by Carolyn at ECP.

--- lib/gas/tools/gasmodel.py
+++ lib/gas/tools/gasmodel.py
@@ -1,7 +1,15 @@
 #! /usr/bin/python
-# File: gasmodel.py
-# Author: Daniel Potter
-# Notes: A python tool to create a gas model file from the command line
+"""
+A python tool to create a gas model file from the command line.
+
+Try the command::
+
+  gasmodel.py --help
+
+..File: gasmodel.py
+
+..Author: Daniel Potter (with docs by PJ)
+"""
 
 import sys
 import os
@@ -10,19 +18,36 @@ from gaspy import *
 
 longOptions = ["help", "model=", "species=", "output=", "lut-file="]
 
-def printUsage():
-    print ""
-    print "Usage: gasmodel.py [--help] [--model=<modelName>] [--species=<speciesList|none>]"
-    print "                   [--lut-file=<LUTFileName>]"
-    print "                   [--output=<luaFile|gas-model.lua>]"
-    print ""
-    print "Examples:"
-    print "$ gasmodel.py --model='thermally perfect gas' --species='N2 N' --output='nitrogen.lua'"
-    print ""
-    print "$ gasmodel.py --model='ideal gas' --species='Ar He' --lut-file='cea-lut-custom.lua.gz' \\"
-    print "              --output='LUT-plus-Ar-He.lua'"
-    print ""
-    return
+usage_string = """
+Use this program to construct a simple or composite gas model for use with
+the simulation codes Eilmer3 of L1d3.
+
+Usage: 
+  gasmodel.py [--help] [--model=<modelName>] [--species=<speciesList|none>] \\
+              [--lut-file=<LUTFileName>] \\
+              [--output=<luaFile|gas-model.lua>]
+
+Input parameters:
+  model   : name of the gas model, may have embedded spaces.
+  species : list of species names (space delimited) in a single string.
+  output  : name of the gas-model file to be written.
+  lut-file: name of the preexisting LUT-gas model file, if relevant.
+
+Examples:
+  $ gasmodel.py --model='thermally perfect gas' --species='N2 N' \\
+                --output='nitrogen.lua'
+
+  $ gasmodel.py --model='ideal gas' --species='Ar He' \\
+                --lut-file='cea-lut-custom.lua.gz' \\
+                --output='LUT-plus-Ar-He.lua'
+
+Notes:
+  If you want a LUT-plus-composite gas model, set up the LUT table
+  externally. Invoke this program, specifying the rest of the species
+  for the composite gas model.  The LUT gas species is prepended to 
+  the composite gas species list.  You will need both gas model files
+  in place to use the resulting LUT-plus-composite gas model.
+"""
 
 def main():
     #
@@ -31,11 +56,11 @@ def main():
     except GetoptError, e:
         print "One (or more) of your command-line options was no good."
         print "    ", e
-        printUsage()
+        print usage_string
         sys.exit(1)
     uoDict = dict(userOptions[0])
     if len(userOptions[0]) == 0 or uoDict.has_key("--help"):
-        printUsage()
+        print usage_string
         sys.exit(0)
     #
     model = uoDict.get("--model", "none")
