616ffc63838f080d89fc8bf28bd81fa092d8b763
Chris James <c.james4@uq.edu.au>
2013-05-06 17:54:32 +1000

    examples/eilmer3/2D/nenzfr-Mach4-nozzle-eq: made some changes so this test works with the new cfg file version of nenzfr

--- examples/eilmer3/2D/nenzfr-Mach4-nozzle-eq/run-mpi-block-marching-mode.sh
+++ examples/eilmer3/2D/nenzfr-Mach4-nozzle-eq/run-mpi-block-marching-mode.sh
@@ -3,9 +3,9 @@
 #
 # Running nenzfr using the new multi-process block-marching mode 
 # that has recently been implemented. Note the two new options
-# needed when running in this mode --nbj and --block-marching.
+# needed when running in this mode (nbj and blockMarching variables
+# that can be seen in the cfg file).
 
 echo "Start time: "; date
-nenzfr.py --gas=air --T1=300 --p1=125.0e3 --Vs=2414 --pe=34.376e6 --area=27.0 --cfile=Bezier-control-pts-t4-m4.data \
-          --nni=600 --nnj=40 --by=1.02 --nbi=60 --nbj=2 --block-marching --max-time=1.0e-3 > LOGFILE
+nenzfr.py --config_file=nenzfr-Mach4-nozzle-eq-block-marching.cfg  > LOGFILE
 echo "Finish time: "; date
