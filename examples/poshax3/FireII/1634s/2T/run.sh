8ced91f47ad6c4e66d264bc7672a529239729cc6
Daniel Potter <daniel.potter@uqconnect.edu.au>
2013-06-13 14:11:45 +0200

    Reduced size of poshax3 FireII plots, and updated sphinx links.

--- examples/poshax3/FireII/1634s/2T/run.sh
+++ examples/poshax3/FireII/1634s/2T/run.sh
@@ -14,8 +14,8 @@ poshax3.x FireII.cfg
 gnuplot profiles.gplot
 
 # 4. Convert to png files
-convert -density 600x600 -quality 90 number_density_profiles.eps number_density_profiles.png
-convert -density 600x600 -quality 90 temperature_profiles.eps temperature_profiles.png 
+convert -density 600x600 -quality 90 number_density_profiles.eps -scale 750 number_density_profiles.png
+convert -density 600x600 -quality 90 temperature_profiles.eps -scale 750 temperature_profiles.png 
 
 # 5. Compute average errors with Panesi solution
 python compute_errors.py 
