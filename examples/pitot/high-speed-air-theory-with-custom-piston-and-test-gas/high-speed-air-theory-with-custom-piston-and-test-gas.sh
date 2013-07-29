efd720c9b36fc2359783fd848297deb4d54f4468
Chris James <c.james4@uq.edu.au>
2013-07-29 13:35:30 +1000

    examples/pitot: added new pitot example using custom piston and custom test gas

---
+++ examples/pitot/high-speed-air-theory-with-custom-piston-and-test-gas/high-speed-air-theory-with-custom-piston-and-test-gas.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# file: high-speed-air-theory-with-custom-piston-and-test-gas.sh
+# Shell script to run the fully theoretical 
+# air example for pitot.
+#Chris James (c.james4@uq.edu.au) - 29/07/13
+
+pitot.py --config_file=high-speed-air-theory-with-custom-piston-and-test-gas.cfg
+
+echo "Done"
+
