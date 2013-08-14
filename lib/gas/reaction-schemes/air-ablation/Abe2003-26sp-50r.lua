d795ee737430dbc8f3037b02cc086e7dbdce7f0f
Nikhil Banerji <nikhil.banerji@epfl.ch>
2013-08-14 12:22:21 +0200

    Bug fixes for reaction-schemes for air-ablation model

--- lib/gas/reaction-schemes/air-ablation/Abe2003-26sp-50r.lua
+++ lib/gas/reaction-schemes/air-ablation/Abe2003-26sp-50r.lua
@@ -1,18 +1,18 @@
--- Abe2003-26sp-50r.lua
+-- Abe1980_26_sp_50r
+--
 -- 
 -- Author: Jeremy Mora
 -- Date: 10-May-2012
 -- Place: EPFL, Lausanne, Switzerland
 --
 -- Species:
--- 'C','C2','C3','CO2','CO','CN','CO_plus','C_plus','C2H2','C2H','CH','H','H2','HCN',
---'CHO','N2','N','NO','N_plus','N2_plus','NO_plus','O2','O','O_plus','O2_plus','e_minus' 
+-- 'C','C2','C3','CO2','CO','CN','CO_plus','C_plus','C2H2','C2H','CH','H','H2','HCN','HCO','N2','N','NO','N_plus','N2_plus','NO_plus','O2','O','O_plus','O2_plus','e_minus' 
 
 scheme_t = {
     update = "chemical kinetic ODE MC",
     temperature_limits = {
-        lower = 20.0,
-        upper = 100000.0
+        lower = 0.000001,
+        upper = 170000.0
     },
     error_tolerance = 0.000001
 }
@@ -22,77 +22,77 @@ scheme_t = {
 reaction{
    'C2 + M <=> C + C + M',
    fr={'Park', A=3.7e14, n=0.0, T_a=69900.0, p_name='C2', p_mode='vibration', s_p=0.3, q_name='C2', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=1.0,HCN=0.0,CHO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=1.0,HCN=0.0,HCO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'C3 + M <=> C + C2 + M',
    fr={'Park', A=1.6e16, n=1.0, T_a=87480.0, p_name='C3', p_mode='vibration', s_p=0.3, q_name='C3', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=1.0,H2=0.0,HCN=0.0,CHO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=0.0,N2_plus=0.0,NO_plus=1.0,O2=0.0,O=1.0,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=1.0,H2=0.0,HCN=0.0,HCO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=0.0,N2_plus=0.0,NO_plus=1.0,O2=0.0,O=1.0,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'C2H2 + M <=> C2H + H + M',
    fr={'Park', A=6.96e39, n=-6.1, T_a=67130.0, p_name='C2H2', p_mode='vibration', s_p=0.3, q_name='C2H2', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,CHO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,HCO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'CN + M <=> C + N + M',
    fr={'Park', A=2.5e14, n=0.0, T_a=87740.0, p_name='CN', p_mode='vibration', s_p=0.3, q_name='CN', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=1.0,HCN=0.0,CHO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=1.0,HCN=0.0,HCO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'CO + M <=> C + O + M',
    fr={'Park', A=2.3e20, n=-1.0, T_a=129000.0, p_name='CO', p_mode='vibration', s_p=0.3, q_name='CO', q_mode='translation'},
-efficiencies={C=1.478,C2=1.0,C3=0.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=0.0,H2=0.0,HCN=0.0,CHO=0.0,N2=1.0,N=1.478,NO=1.0,N_plus=0.0,N2_plus=0.0,NO_plus=0.0,O2=1.0,O=1.478,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=1.478,C2=1.0,C3=0.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=0.0,H2=0.0,HCN=0.0,HCO=0.0,N2=1.0,N=1.478,NO=1.0,N_plus=0.0,N2_plus=0.0,NO_plus=0.0,O2=1.0,O=1.478,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'CO2 + M <=> CO + O + M',
    fr={'Park', A=3.5e14, n=0.0, T_a=52525.0, p_name='CO2', p_mode='vibration', s_p=0.3, q_name='CO2', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,CHO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,HCO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'H2 + M <=> H + H + M',
    fr={'Park', A=2.2e14, n=0.0, T_a=48300.0, p_name='H2', p_mode='vibration', s_p=0.3, q_name='H2', q_mode='translation'},
-efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=2.5,HCN=0.0,CHO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=0.0,H=1.0,H2=2.5,HCN=0.0,HCO=0.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
-   'CHO + M <=> H + CO + M',
-   fr={'Park', A=1.87e17, n=-1.0, T_a=8560.5, p_name='CHO', p_mode='vibration', s_p=0.3, q_name='CHO', q_mode='translation'},
-   efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,CHO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
+   'HCO + M <=> H + CO + M',
+   fr={'Park', A=1.87e17, n=-1.0, T_a=8560.5, p_name='HCO', p_mode='vibration', s_p=0.3, q_name='HCO', q_mode='translation'},
+   efficiencies={C=1.0,C2=1.0,C3=1.0,CO2=1.0,CO=1.0,CN=1.0,CO_plus=1.0,C_plus=1.0,C2H2=1.0,C2H=1.0,CH=1.0,H=1.0,H2=1.0,HCN=1.0,HCO=1.0,N2=1.0,N=1.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=1.0,O_plus=1.0,O2_plus=1.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'N2 + M <=> N + N + M',
    fr={'Park', A=7.0e21, n=-1.60, T_a=113200.0, p_name='N2', p_mode='vibration', s_p=0.3, q_name='N2', q_mode='translation'},
-   efficiencies={C=4.286,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=1.0,H=4.286,H2=1.0,HCN=0.0,CHO=0.0,N2=1.0,N=4.286,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=4.286,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
+   efficiencies={C=4.286,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=1.0,H=4.286,H2=1.0,HCN=0.0,HCO=0.0,N2=1.0,N=4.286,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=4.286,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'NO + M <=> N + O + M',
    fr={'Park', A=5.0e15, n=0.0, T_a=75500.0, p_name='NO', p_mode='vibration', s_p=0.3, q_name='NO', q_mode='translation'},
-   efficiencies={C=22.0,C2=1.0,C3=0.0,CO2=22.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=22.0,H2=0.0,HCN=0.0,CHO=0.0,N2=1.0,N=22.0,NO=22.0,N_plus=0.0,N2_plus=0.0,NO_plus=0.0,O2=1.0,O=22.0,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
+   efficiencies={C=22.0,C2=1.0,C3=0.0,CO2=22.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=0.0,C2H2=0.0,C2H=0.0,CH=0.0,H=22.0,H2=0.0,HCN=0.0,HCO=0.0,N2=1.0,N=22.0,NO=22.0,N_plus=0.0,N2_plus=0.0,NO_plus=0.0,O2=1.0,O=22.0,O_plus=0.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'O2 + M <=> O + O + M',
    fr={'Park', A=2.0e21, n=-1.5, T_a=59360.0, p_name='O2', p_mode='vibration', s_p=0.3, q_name='O2', q_mode='translation'},
-efficiencies={C=5.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=1.0,H=5.0,H2=1.0,HCN=0.0,CHO=0.0,N2=1.0,N=5.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=5.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
+efficiencies={C=5.0,C2=1.0,C3=1.0,CO2=0.0,CO=1.0,CN=1.0,CO_plus=0.0,C_plus=1.0,C2H2=0.0,C2H=1.0,CH=1.0,H=5.0,H2=1.0,HCN=0.0,HCO=0.0,N2=1.0,N=5.0,NO=1.0,N_plus=1.0,N2_plus=1.0,NO_plus=1.0,O2=1.0,O=5.0,O_plus=1.0,O2_plus=0.0,e_minus=0.0},
    ec={model='from CEA curves',iT=0}
 }
 
@@ -111,19 +111,19 @@ reaction{
 }
 
 reaction{
-   'CH + O2 <=> CHO + O',
-   fr={'Arrhenius', A=6.71e13, n=0.0, T_a=0.0},
+   'CH + O2 <=> HCO + O',
+   fr={'Arrhenius', A=6.71e13, n=0.0, T_a=0.000001},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
    'C2H + O <=> CH + CO',
-   fr={'Arrhenius', A=5.0e13, n=0.0, T_a=0.0},
+   fr={'Arrhenius', A=5.0e13, n=0.0, T_a=0.000001},
    ec={model='from CEA curves',iT=0}
 }
 
 reaction{
-   'C2H + O2 <=> CHO + CO',
+   'C2H + O2 <=> HCO + CO',
    fr={'Arrhenius', A=1.0e13, n=0.0, T_a=3524.9},
    ec={model='from CEA curves',iT=0}
 }
@@ -135,7 +135,7 @@ reaction{
 }
 
 reaction{
-   'C2H2 + O2 <=> CHO + CHO',
+   'C2H2 + O2 <=> HCO + HCO',
    fr={'Arrhenius', A=3.98e12, n=0.0, T_a=14106.7},
    ec={model='from CEA curves',iT=0}
 }
@@ -207,8 +207,8 @@ reaction{
 }
 
 reaction{
-   'CHO + H <=> CO + H2',
-   fr={'Arrhenius', A=7.34e13, n=0.0, T_a=0.0},
+   'HCO + H <=> CO + H2',
+   fr={'Arrhenius', A=7.34e13, n=0.0, T_a=0.000001},
    ec={model='from CEA curves',iT=0}
 }
 
