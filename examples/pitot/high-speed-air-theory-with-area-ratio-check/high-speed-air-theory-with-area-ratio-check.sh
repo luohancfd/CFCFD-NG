69eba67448171c19b59e802aafec9cf595e8e968
Chris James <c.james4@uq.edu.au>
2013-08-22 14:14:30 +1000

    examples/pitot: added new area ratio check example

---
+++ examples/pitot/high-speed-air-theory-with-area-ratio-check/high-speed-air-theory-with-area-ratio-check.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# file: high-speed-air-theory-with-area-ratio-check.sh
+# Shell script to run the fully theoretical 
+# air example for pitot that also uses the area ratio check function.
+#Chris James (c.james4@uq.edu.au) - 22/08/13
+
+pitot.py --config_file=high-speed-air-theory-with-area-ratio-check.cfg
+
+echo "Done"
+
