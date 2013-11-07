9f3a555705eb3f6c424f9f97541453785959aa54
Daniel Potter (laptop) <daniel.potter@uqconnect.edu.au>
2013-11-07 11:58:29 +0530

    examples/photaura: updated input file format for argon spectra

--- examples/photaura/equilibrium-spectra/Argon/post-shock/argon-radiators.py
+++ examples/photaura/equilibrium-spectra/Argon/post-shock/argon-radiators.py
@@ -1,22 +1,21 @@
-# A test input.py file for defining a radiation model
-
-# first the spectral data
+# first the spectral model
 gdata.spectral_model = "photaura"
-gdata.lambda_min = 100.0
-gdata.lambda_max = 2000.0
-gdata.spectral_points = 190000
+gdata.lambda_min = 1.0e7 / 150000.0
+gdata.lambda_max = 1.0e7 / 1000.0
+gdata.spectral_points = 100
+gdata.adaptive_spectral_grid = True
 
 # now the radiators
-# dictionary of indices
 
-species = [ 'Ar', 'Ar_plus', 'e_minus' ]
-radiators = [ 'Ar', 'Ar_plus', 'e_minus' ]
+params = {
+"species"               : [ 'Ar', 'Ar_plus', 'e_minus' ],
+"radiators"             : [ 'Ar', 'Ar_plus', 'e_minus' ],
+"QSS_radiators"         : [],
+"no_emission_radiators" : [],
+"atomic_level_source"   : "NIST_ASD",
+"atomic_line_source"    : "NIST_ASD",
+"atomic_PICS_source"    : "TOPBase"
+}
 
-for rad_name in radiators:
-    rad = gdata.request_radiator(rad_name)
-    rad.default_data()
-    rad.isp = species.index(rad_name)
-    if rad.type=="atomic_radiator":
-        rad.line_set = rad.available_line_sets["all_lines"]
+declare_radiators( params, gdata )
 
-# thats all we need to do!
