b3faeaf7f0a5e94eea8a5c89cebf2d1aa0fc5204
Wilson Chan <y.chan1@uq.edu.au>
2013-12-03 17:55:33 +1000

    Nozzle optimiser: Added example for the optimisation of the Mach 4 nozzle contour
    Brought scripts for the nozzle optimiser up to date with the current nenzfr/eilmer
    scripts. This example demonstrates the optimisation of the contour of the T4
    Mach 4 nozzle. The boundary layers in the nozzle were assumed to be laminar and
    the flow through the nozzle to be in chemical equilibrium.

---
+++ examples/eilmer3/2D/nenzfr-optimisation-Mach4-nozzle-eq/run-nenzfr-mpi-block-marching-mode.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+# run-nenzfr-mpi-block-marching-mode.sh
+#
+# Running nenzfr using the new multi-process block-marching mode 
+# that has recently been implemented. Note the two new options
+# needed when running in this mode (nbj and blockMarching variables
+# that can be seen in the cfg file).
+
+echo "Start time: "; date
+nenzfr.py --config_file=nenzfr-Mach4-nozzle-eq-block-marching.cfg 
+echo "Finish time: "; date
--- examples/eilmer3/2D/nozzle-optimisation/run-nenzfr-in-mpi-block-marching-mode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-# run-nenzfr-in-mpi-block-marching-mode.sh
-
-echo "Start time: "; date
-nenzfr.py --gas="air" --T1=300 --p1=131.0e3 --Vs=1644.0 --pe=8.32e6 --chem="eq" \
-          --area=129.72 --cfile=Bezier-control-pts-t4-m7.data --nni=300 --nnj=40 --by=1.002 --bx=1.5 \
-          --nbi=30 --nbj=4 --block-marching --max-time=2.0e-3 --max-step=1000000 --BLTrans=0.00
-echo "Finish time: "; date
-
