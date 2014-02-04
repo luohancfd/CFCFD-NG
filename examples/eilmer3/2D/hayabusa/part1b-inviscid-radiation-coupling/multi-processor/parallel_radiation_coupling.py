83ec1cd8feadde352ca04152709a93266196d5e9
Elise Fahy <e.fahy@uq.edu.au>
2014-02-04 14:11:56 +1000

    Slight edit to radiation/flowfield coupling script.
    This script, first included by Dan, couples the radiation and flowfield
    solvers by manually choosing the number of steps per iteration. Work is
    ongoing to automate this further, and run the coupling based on some
    convergence parameter. Stay tuned.

--- examples/eilmer3/2D/hayabusa/part1b-inviscid-radiation-coupling/multi-processor/parallel_radiation_coupling.py
+++ examples/eilmer3/2D/hayabusa/part1b-inviscid-radiation-coupling/multi-processor/parallel_radiation_coupling.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+# parallel_radiation_coupling.py
+# an attempt for coupling radiation and flowfield solvers through a
+# simple python script. at present, the steps per iteration are selected
+# manually.
+
 import os
 
 ifile = open("hayabusa.control")
@@ -10,11 +16,13 @@ for line in lines:
     if tks[0]=="max_step": initial_max_step = int(tks[2])
 
 prev_max_step = initial_max_step
-max_steps = [ 100, 100, 1000, 1000, 1000, 1000, 1000 ]
+max_steps = [ 100, 100, 100, 100, 100, 100, 100 ]
+# be careful with selection of max_steps - too large will cause the
+# system to become unstable and the simulation will crash.
 
 for iteration,max_step in enumerate(max_steps):
     print "Performing flowfield-radiation coupling iteration %d" % iteration
-    os.system("e3rad.exe -f hayabusa -r > E3RAD_LOG_%d" % iteration)
+    os.system("e3rad.exe -f hayabusa -t %d -r > E3RAD_LOG_%d" % (iteration,iteration) )
     os.system("sed -ie 's/max_step = %d/max_step = %d/g' hayabusa.control" % ( prev_max_step, max_step ) ) 
     os.system("mpirun -np 4 e3mpi.exe -f hayabusa -t %d -r > E3MPI_LOG_%d" % (iteration+1,iteration))
     prev_max_step = max_step
