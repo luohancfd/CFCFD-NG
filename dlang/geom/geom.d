5e02f0e46893e4733b040c63d0ecd5f3e6394a27
Peter Jacobs <peterj@mech.uq.edu.au>
2014-06-16 15:45:29 +1000

    dlang: dlang: Vector3 binary operators and combined assignment operators.

--- dlang/geom/geom.d
+++ dlang/geom/geom.d
@@ -1,5 +1,5 @@
 /**
- * geom.d  Geometric primitives for our 3D world.
+ * geom.d  Geometric (vector) primitives for our 3D world.
  *
  * Author: Peter J.
  * Version: 2014-06-16 first cut.
@@ -16,7 +16,7 @@ class Vector3 {
 	_p[0] = _p[1] = _p[2] = 0.0;
     }
 
-    this(double[] p) {
+    this(in double[] p) {
 	_p = new double[3];
 	switch ( p.length ) {
 	case 0: _p[0] = _p[1] = _p[2] = 0.0; break;
@@ -49,27 +49,87 @@ class Vector3 {
 	return "Vector3(" ~ to!string(_p) ~ ")";
     }
 
-    Vector3 opBinary(string op)(Vector3 rhs) if (op == "+") {
+    // Some binary operators, at least those that make sense.
+    Vector3 opBinary(string op)(in Vector3 rhs) if (op == "+") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] + rhs._p[];
 	return result;
     }
+
+    Vector3 opBinary(string op)(in Vector3 rhs) if (op == "-") {
+	Vector3 result = new Vector3();
+	result._p[] = this._p[] - rhs._p[];
+	return result;
+    }
+
+    Vector3 opBinary(string op)(in double rhs) if (op == "*") {
+	Vector3 result = new Vector3();
+	result._p[] = this._p[] * rhs;
+	return result;
+    }
+
+    Vector3 opBinaryRight(string op)(in double lhs) if (op == "*") {
+	Vector3 result = new Vector3();
+	result._p[] = this._p[] * lhs;
+	return result;
+    }
+
+    Vector3 opBinary(string op)(in double rhs) if (op == "/") {
+	Vector3 result = new Vector3();
+	result._p[] = this._p[] / rhs;
+	return result;
+    }
+
+    // Combined assignment operators
+    ref Vector3 opOpAssign(string op)(in Vector3 rhs) if (op == "+") {
+	this._p[] += rhs._p[];
+	return this;
+    }
+
+    ref Vector3 opOpAssign(string op)(in Vector3 rhs) if (op == "-") {
+	this._p[] -= rhs._p[];
+	return this;
+    }
+
+    ref Vector3 opOpAssign(string op)(in double rhs) if (op == "*") {
+	this._p[] *= rhs;
+	return this;
+    }
+
+    ref Vector3 opOpAssign(string op)(in double rhs) if (op == "/") {
+	this._p[] /= rhs;
+	return this;
+    }
 } // end class Vector3
+
 
 unittest {
     // Check that we have separate data with the correct values.
     Vector3 a = new Vector3([1.0, 2.2, 3.0]);
     Vector3 b = new Vector3(1.0);
-    assert( a.x == 1.0, "a.x failed" );
-    assert( a.y == 2.2, "a.y failed" );
-    assert( a.z == 3.0, "a.z failed" );
-    assert( a.x == b.x, "a.x == b.x failed" );
-    assert( b.y == 0.0, "b.y failed" );
-    assert( b.z == 0.0, "b.z failed" );
+    assert(a.x == 1.0, "a.x failed");
+    assert(a.y == 2.2, "a.y failed");
+    assert(a.z == 3.0, "a.z failed");
+    assert(a.x == b.x, "a.x == b.x failed");
+    assert(b.y == 0.0, "b.y failed");
+    assert(b.z == 0.0, "b.z failed");
 
     Vector3 c = a + b;
-    assert( c.y == a.y+b.y );
+    assert(c.y == a.y+b.y, "vector addition failed");
+    c = a - b;
+    assert(c.y == a.y-b.y, "vector subtraction failed");
     Vector3 d = a.dup;
     a.y = 99.0;
-    assert( a.y == 99.0 && d.y == 2.2 );   
+    assert(a.y == 99.0 && d.y == 2.2, "dup followed by vector addition failed");
+
+    Vector3 e = a * 2.0;
+    Vector3 f = 3.0 * d;
+    assert(e.z == 6.0 && f.z == 9.0, "scalar multiply failed");
+    Vector3 g = d / 3.0;
+    assert(g.z == 1.0, "scalar division failed");
+
+    g += f;
+    assert(g.z == 10.0, "plus-assign failed");
+    g /= 2.0;
+    assert(g.z == 5.0, "divide-assign failed");
 }
--- dlang/geom/geom_demo.d
+++ dlang/geom/geom_demo.d
@@ -13,11 +13,20 @@ void main()
     writeln("Begin demonstration of the geometric primitives.");
     Vector3 a = new Vector3([1.0, 2.2, 3.0]);
     Vector3 b = new Vector3(1.0);
-    writeln("a = ", a.toString(), ", b = ", b.toString());
+    writeln("a = ", a, ", b = ", b);
     writeln("Done.");
     Vector3 c = a + b;
-    writeln("c = a + b = ", c.toString());
+    writeln("c = a + b = ", c);
     Vector3 d = a.dup;
     a.y = 99.0;
-    writeln("a = ", a.toString(), ", d = ", d.toString());   
+    writeln("a = ", a, ", d = ", d); 
+    Vector3 e = a * 2.0;
+    Vector3 f = 3 * d; // int promoted to double OK
+    writeln("e = ", e, " f = ", f);
+    Vector3 g = d / 3.0;
+    writeln("g = ", g);
+    g += f;
+    writeln("g += f -> ", g);
+    g /= 2.0;
+    writeln("g /= 2.0 -> ", g);
 }
