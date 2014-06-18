b026f971dc44f7463f484990b2c7a9358d10da1a
Peter Jacobs <peterj@mech.uq.edu.au>
2014-06-18 11:37:40 +1000

    dlang: Make operator methods const so we can pass around Vector3 expressions
    This is needed, as we start the port of the C++ geometry functions that use
    temporary expressions built with Vector3 objects.

--- dlang/geom/geom.d
+++ dlang/geom/geom.d
@@ -54,49 +54,51 @@ class Vector3 {
     }
 
     // Some operators, at least those that make sense.
-    Vector3 opUnary(string op)() if (op == "+") {
+    // Note that they make new values and do not alter the original object.
+    // Note 2, the const does not refer to the returned (new) object.
+    const Vector3 opUnary(string op)() if (op == "+") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[];
 	return result;
     }
 
-    Vector3 opUnary(string op)() if (op == "-") {
+    const Vector3 opUnary(string op)() if (op == "-") {
 	Vector3 result = new Vector3();
 	result._p[] = - this._p[];
 	return result;
     }
 
-    Vector3 opBinary(string op)(in Vector3 rhs) if (op == "+") {
+    const Vector3 opBinary(string op)(in Vector3 rhs) if (op == "+") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] + rhs._p[];
 	return result;
     }
 
-    Vector3 opBinary(string op)(in Vector3 rhs) if (op == "-") {
+    const Vector3 opBinary(string op)(in Vector3 rhs) if (op == "-") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] - rhs._p[];
 	return result;
     }
 
-    Vector3 opBinary(string op)(in double rhs) if (op == "*") {
+    const Vector3 opBinary(string op)(in double rhs) if (op == "*") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] * rhs;
 	return result;
     }
 
-    Vector3 opBinaryRight(string op)(in double lhs) if (op == "*") {
+    const Vector3 opBinaryRight(string op)(in double lhs) if (op == "*") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] * lhs;
 	return result;
     }
 
-    Vector3 opBinary(string op)(in double rhs) if (op == "/") {
+    const Vector3 opBinary(string op)(in double rhs) if (op == "/") {
 	Vector3 result = new Vector3();
 	result._p[] = this._p[] / rhs;
 	return result;
     }
 
-    // Combined assignment operators
+    // Combined assignment operators do change the original object.
     ref Vector3 opOpAssign(string op)(in Vector3 rhs) if (op == "+") {
 	this._p[] += rhs._p[];
 	return this;
@@ -118,6 +120,10 @@ class Vector3 {
     }
 
     // Other vector-specific operations.
+
+    /**
+     * Scales the vector to unit magnitude.
+     */
     ref Vector3 normalize() {
         double magnitude = abs(this);
 	if ( magnitude > 0.0 ) {
@@ -129,7 +135,11 @@ class Vector3 {
 	return this;
     }
 } // end class Vector3
-    
+
+
+/**
+ * Returns the scalar dot product of two vectors.
+ */   
 double dot(in Vector3 v1, in Vector3 v2) {
     double result = 0.0;
     // Maybe we should be careful with underflow and overflow...
@@ -137,14 +147,23 @@ double dot(in Vector3 v1, in Vector3 v2) {
     return result;
 }
 
+/**
+ * Returns magnitude of the vector.
+ */
 double abs(in Vector3 v) {
     return sqrt(v.dot(v));
 }
 
+/**
+ * Returns a unit vector in the same direction as v.
+ */
 Vector3 unit(in Vector3 v) {
     return new Vector3(v).normalize();
 }
 
+/**
+ * Vector cross product.
+ */
 Vector3 cross(in Vector3 v1, in Vector3 v2) {
     Vector3 v3 = new Vector3();
     v3._p[0] = v1._p[1] * v2._p[2] - v2._p[1] * v1._p[2];
@@ -153,6 +172,41 @@ Vector3 cross(in Vector3 v1, in Vector3 v2) {
     return v3;
 }
 
+// Transform functions used to reorient vector values in the CFD codes.
+
+/**
+ * Rotate v from the global xyz coordinate system into the local frame
+ * defined by the orthogonal unit vectors n,t1,t2.
+ *
+ * We assume, without checking, that these vectors do nicely define 
+ * such a local system.
+ */
+void to_local_frame(ref Vector3 v, in Vector3 n, in Vector3 t1, in Vector3 t2)
+{
+    double v_x = dot(v, n); // normal component
+    double v_y = dot(v, t1); // tangential component 1
+    double v_z = dot(v, t2); // tangential component 2
+    v._p[0] = v_x;
+    v._p[1] = v_y;
+    v._p[2] = v_z;
+}
+
+/**
+ * Rotate v back into the global coordinate system.
+ */
+void to_xyz_frame(ref Vector3 v, in Vector3 n, in Vector3 t1, in Vector3 t2)
+{
+    double v_x = v._p[0]*n._p[0] + v._p[1]*t1._p[0] + v._p[2]*t2._p[0]; // global-x
+    double v_y = v._p[0]*n._p[1] + v._p[1]*t1._p[1] + v._p[2]*t2._p[1]; // global-y
+    double v_z = v._p[0]*n._p[2] + v._p[1]*t1._p[2] + v._p[2]*t2._p[2]; // global-z
+    v._p[0] = v_x;
+    v._p[1] = v_y;
+    v._p[2] = v_z;
+}
+
+/**
+ * Returns true is all of the components are approximately equal.
+ */
 bool approxEqualVectors(in Vector3 v1, in Vector3 v2) {
     return (approxEqual(v1._p[0], v2._p[0]) && 
 	    approxEqual(v1._p[1], v2._p[1]) &&
@@ -201,4 +255,61 @@ unittest {
     Vector3 z = cross(x,y);
     Vector3 zref = new Vector3(0.0,0.0,1.0);
     assert(approxEqualVectors(z, zref), "cross product fail");
+
+    Vector3 n = unit(new Vector3(1.0,1.0,0.0));
+    Vector3 t1 = unit(new Vector3(-1.0,1.0,0.0));
+    Vector3 t2 = cross(n, t1);
+    Vector3 h = new Vector3(1.0,0.0,1.0);
+    Vector3 h_ref = new Vector3(h);
+    to_local_frame(h, n, t1, t2);
+    assert(approxEqualVectors(h, new Vector3(sqrt(1.0/2.0), -sqrt(1.0/2.0), 1.0)),
+	   "to_local_frame fail");
+    to_xyz_frame(h, n, t1, t2);
+    assert(approxEqualVectors(h, h_ref), "to_xyz_frame fail");
+}
+
+
+//------------------------------------------------------------------------
+// Geometry functions that make use of Vector3 objects...
+
+/**
+ * Project the point q onto the plane, along the vector qr.
+ */
+int project_onto_plane(ref Vector3 q, in Vector3 qr,
+		       in Vector3 a, in Vector3 b, in Vector3 c)
+{
+    // See Section 7.2 in
+    // J. O'Rourke (1998)
+    // Computational Geometry in C (2nd Ed.)
+    // Cambridge Uni Press 
+    // See 3D CFD workbook p17, 25-Jan-2006
+
+    // Define a plane Ax + By + Cz = D using the corners of the triangle abc.
+    Vector3 N = cross(a-c, b-c); // N = Vector3(A, B, C)
+    double D = dot(a, N);
+
+    double numer = D - dot(q, N);
+    double denom = dot(qr, N);
+
+    double tol = 1.0e-12;  // floating point tolerance
+    if ( fabs(denom) < tol ) {
+	if ( fabs(numer) < tol ) {
+	    return 1;  // qr is parallel to the plane and q is on the plane
+	} else {
+	    return 2;  // qr is parallel to the plane and q is off the plane
+	}
+    } else {
+	q = q + (numer/denom) * qr;
+	return 0;  // point q has been projected onto the plane.
+    } 
+} // end project_onto_plane()
+
+unittest {
+    Vector3 a = new Vector3(1.0, 0.0, 0.0); // plane through a,b,c
+    Vector3 b = new Vector3(1.0, 1.0, 0.0);
+    Vector3 c = new Vector3(0.5, 0.0, 0.0);
+    Vector3 qr = new Vector3(3.0, 3.0, -3.0); // direction
+    Vector3 q = new Vector3(0.0, 0.0, 1.0); // start point
+    int flag =  project_onto_plane(q, qr, a, b, c);
+    assert(approxEqualVectors(q, new Vector3(1.0,1.0,0.0)), "project_onto_plane fail");
 }
--- dlang/geom/geom_demo.d
+++ dlang/geom/geom_demo.d
@@ -35,4 +35,24 @@ void main()
     Vector3 y = new Vector3(0.0, 1.0, 0.0);
     Vector3 z = cross(x,y);
     writeln("z = ", z);
+
+    Vector3 n = unit(new Vector3(1.0,1.0,0.0));
+    Vector3 t1 = unit(new Vector3(-1.0,1.0,0.0));
+    Vector3 t2 = cross(n, t1);
+    Vector3 h = new Vector3(1.0,0.0,1.0);
+    Vector3 h_ref = new Vector3(h);
+    writeln("original h = ", h);
+    to_local_frame(h, n, t1, t2);
+    writeln("in local frame h = ", h);
+    to_xyz_frame(h, n, t1, t2);
+    writeln("back to global frame h = ", h);
+
+    writeln("Try out geometric functions that build on Vector3 objects.");
+    a = new Vector3(1.0, 0.0, 0.0); // plane through a,b,c
+    b = new Vector3(1.0, 1.0, 0.0);
+    c = new Vector3(0.5, 0.0, 0.0);
+    Vector3 qr = new Vector3(3.0, 3.0, -3.0); // direction
+    Vector3 q = new Vector3(0.0, 0.0, 1.0); // start point
+    int flag =  project_onto_plane(q, qr, a, b, c);
+    writeln("projected point q = ", q);
 }
