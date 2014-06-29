fa3bbd1f0ea5e537f16b90f7a89b77804cf656c9
Peter Jacobs <peterj@mech.uq.edu.au>
2014-06-29 22:22:29 +1000

    dlang bare-bones linear algebra: added more convenience methods and unit tests.
    Mmmm, this simple Matrix class is starting to bloat...

--- dlang/nm/bbla.d
+++ dlang/nm/bbla.d
@@ -6,9 +6,12 @@
  * Version: 2014-06-28, just enough to get our CFD code going.
  */
 
+module bbla;
+
 import std.conv;
 import std.algorithm;
 import std.math;
+import std.exception;
 
 class Matrix {
     size_t _nrows;
@@ -36,12 +39,46 @@ class Matrix {
 		_data[row][col] = other._data[row][col];
     }
 
-    this(in double[] vec) {
-	this(vec.length, 1);
-	foreach(row; 0 .. _nrows) _data[row][0] = vec[row];
+    this(in double[] vec, string orient="column") 
+    {
+	if ( orient == "column" ) {
+	    this(vec.length, 1);
+	    foreach(row; 0 .. _nrows) _data[row][0] = vec[row];
+	} else {
+	    this(1, vec.length);
+	    foreach(col; 0 .. _ncols) _data[0][col] = vec[col];
+	}
+    }
+
+    this(in float[] vec, string orient="column") {
+	double[] my_vec;
+	my_vec.length = vec.length;
+	foreach(i; 0 .. my_vec.length) my_vec[i] = vec[i]; 
+	this(my_vec, orient);
+    }
+
+    this(in int[] vec, string orient="column") {
+	double[] my_vec;
+	my_vec.length = vec.length;
+	foreach(i; 0 .. my_vec.length) my_vec[i] = vec[i]; 
+	this(my_vec, orient);
     }
 
     this(in double[][] other) {
+	this(other.length, other[0].length);
+	foreach(row; 0 .. _nrows)
+	    foreach(col; 0 .. _ncols)
+		_data[row][col] = other[row][col];
+    }
+
+    this(in float[][] other) {
+	this(other.length, other[0].length);
+	foreach(row; 0 .. _nrows)
+	    foreach(col; 0 .. _ncols)
+		_data[row][col] = other[row][col];
+    }
+
+    this(in int[][] other) {
 	this(other.length, other[0].length);
 	foreach(row; 0 .. _nrows)
 	    foreach(col; 0 .. _ncols)
@@ -61,31 +98,73 @@ class Matrix {
     }
 
     ref double opIndexOpAssign(string op)(double c, size_t row, size_t col)
-	if ( op == "+" )
+	if ( op == "+" || op == "-" || op == "*" || op == "/" )
     {
-	_data[row][col] += c;
+	static if ( op == "+" )
+	    _data[row][col] += c;
+	else if ( op == "-" )
+	    _data[row][col] -= c;
+	else if ( op == "*" )
+	    _data[row][col] *= c;
+	else if ( op == "/" )
+	    _data[row][col] /= c;
 	return _data[row][col];
     }
 
-    ref double opIndexOpAssign(string op)(double c, size_t row, size_t col)
-	if ( op == "-" )
+    Matrix opBinary(string op)(in Matrix rhs)
+	if ( op == "+" || op == "-" )
     {
-	_data[row][col] -= c;
-	return _data[row][col];
+	enforce(_nrows == rhs._nrows && _ncols == rhs._ncols,
+		"incompatible matrices");
+	Matrix result = new Matrix(_nrows, _ncols);
+	foreach(row; 0 .. _nrows) {
+	    foreach(col; 0 .. _ncols) {
+		static if ( op == "+" ) {
+		    result._data[row][col] = _data[row][col] + rhs._data[row][col];
+		} else if ( op == "-" ) {
+		    result._data[row][col] = _data[row][col] - rhs._data[row][col];
+		}
+	    }
+	}
+	return result;
     }
 
-    ref double opIndexOpAssign(string op)(double c, size_t row, size_t col)
-	if ( op == "*" )
+    Matrix opBinary(string op)(double rhs)
+	if ( op == "+" || op == "-" || op == "*" || op == "/" )
     {
-	_data[row][col] *= c;
-	return _data[row][col];
+	Matrix result = new Matrix(_nrows, _ncols);
+	foreach(row; 0 .. _nrows) {
+	    foreach(col; 0 .. _ncols) {
+		static if ( op == "+" ) {
+		    result._data[row][col] = _data[row][col] + rhs;
+		} else if ( op == "-" ) {
+		    result._data[row][col] = _data[row][col] - rhs;
+		} else if ( op == "*" ) {
+		    result._data[row][col] = _data[row][col] * rhs;
+		} else if ( op == "/" ) {
+		    result._data[row][col] = _data[row][col] / rhs;
+		}
+	    }
+	}
+	return result;
     }
 
-    ref double opIndexOpAssign(string op)(double c, size_t row, size_t col)
-	if ( op == "/" )
+    Matrix opBinaryRight(string op)(double lhs)
+	if ( op == "+" || op == "-" || op == "*" )
     {
-	_data[row][col] /= c;
-	return _data[row][col];
+	Matrix result = new Matrix(_nrows, _ncols);
+	foreach(row; 0 .. _nrows) {
+	    foreach(col; 0 .. _ncols) {
+		static if ( op == "+" ) {
+		    result._data[row][col] = lhs + _data[row][col];
+		} else if ( op == "-" ) {
+		    result._data[row][col] = lhs - _data[row][col];
+		} else if ( op == "*" ) {
+		    result._data[row][col] = lhs * _data[row][col];
+		}
+	    }
+	}
+	return result;
     }
 
     override string toString() {
@@ -103,10 +182,65 @@ class Matrix {
 	return s;
     }
 
-    void swap_rows(size_t i1, size_t i2)
-    {
+    void swapRows(size_t i1, size_t i2) {
 	swap(_data[i1], _data[i2]);
     }
+
+    double[] getColumn(size_t col) {
+	double[] my_column;
+	my_column.length = nrows;
+	foreach(row; 0 .. nrows) my_column[row] = _data[row][col];
+	return my_column;
+    }
+
+    double[] getRow(size_t row) {
+	return _data[row].dup;
+    }
+
+    // Maybe there is a way of using built-in slices here.
+    Matrix sliceDup(size_t row0, size_t row1, size_t col0, size_t col1)
+    {
+	enforce((row0 < row1) && (row1 <= this._nrows) &&
+		(col0 < col1) && (col1 <= this._ncols),
+		new Error("invalid subrange"));
+	Matrix sub_matrix = new Matrix(row1-row0, col1-col0);
+	foreach(row; row0 .. row1) {
+	    foreach(col; col0 .. col1) {
+		sub_matrix[row-row0, col-col0] = this._data[row][col];
+	    }
+	}
+	return sub_matrix;
+    }
+
+    void sliceAssign(double c, size_t row0, size_t row1, size_t col0, size_t col1)
+    {
+	enforce((row0 < row1) && (row1 <= this._nrows) &&
+		(col0 < col1) && (col1 <= this._ncols),
+		new Error("invalid subrange"));
+	foreach(row; row0 .. row1) {
+	    foreach(col; col0 .. col1) {
+		this._data[row][col] = c;
+	    }
+	}
+    }
+
+} // end class Matrix
+
+
+bool approxEqualMatrix(in Matrix a, in Matrix b)
+{
+    if ( a.nrows != b.nrows ) return false;
+    if ( a.ncols != b.ncols ) return false;
+    bool is_equal = true;
+    foreach(row; 0 .. a.nrows) {
+	foreach(col; 0 .. a.ncols) {
+	    if ( !approxEqual(a[row,col], b[row,col]) ) {
+		is_equal = false;
+		break;
+	    }
+	}
+    }
+    return is_equal;
 }
 
 Matrix zeros(size_t rows, size_t cols)
@@ -160,12 +294,39 @@ Matrix dot(in Matrix a, in Matrix b)
     return c;
 }
 
+unittest {
+    Matrix a = eye(3);
+    assert(approxEqualMatrix(a, new Matrix([[1,0,0],[0,1,0],[0,0,1]])),
+	   "eye(), indentity matrix");
+    Matrix b = zeros(2,3);
+    Matrix c = transpose(b);
+    b[1,2] = 99.0;
+    c[0,0] = 1.0; c[1,1] = 1.0;
+    assert(approxEqualMatrix(b, new Matrix([[0,0,0],[0,0,99]])),
+	   "zeros and assign");
+    assert(approxEqualMatrix(c, new Matrix([[1,0],[0,1],[0,0]])),
+	   "transpose and assign");
+
+    Matrix e = new Matrix([1.0, 2.0, 3.0]);
+    assert(approxEqualMatrix(e, new Matrix([[1],[2],[3]])),
+	   "column vector entered as array");
+    Matrix e2 = new Matrix([1, 2, 3], "row");
+    assert(approxEqualMatrix(e2, new Matrix([[1,2,3]])),
+	   "row vector entered as array of int");
+
+    Matrix f = new Matrix([[1.0,2.0,3.0],[4.0,5.0,6.0]]);
+    Matrix g = dot(f,c);
+    assert(approxEqualMatrix(g, new Matrix([[1,2],[4,5]])),
+	   "dot product");
+}
+
+
 /**
  * Perform Gauss-Jordan elimination on an augmented matrix.
  * c = [A|b] such that the mutated matrix becomes [I|x]
  * where x is the solution vector(s) to A.x = b
  */
-void gauss_jordan_elimination(ref Matrix c, double very_small_value=1.0e-16)
+void gaussJordanElimination(ref Matrix c, double very_small_value=1.0e-16)
 {
     if (c.ncols < c.nrows) {
 	throw new Exception("too few columns supplied");
@@ -179,7 +340,7 @@ void gauss_jordan_elimination(ref Matrix c, double very_small_value=1.0e-16)
 	if ( abs(c[p,j]) < very_small_value ) {
 	    throw new Exception("matrix is essentially singular");
 	}
-	c.swap_rows(p,j);
+	c.swapRows(p,j);
 	// Scale row j to get unity on the diagonal.
 	double cjj = c[j,j];
 	foreach(col; 0 .. c.ncols) c[j,col] /= cjj;
@@ -190,4 +351,22 @@ void gauss_jordan_elimination(ref Matrix c, double very_small_value=1.0e-16)
 	    foreach(col; 0 .. c.ncols) c[i,col] -= cij * c[j,col]; 
 	}
     } // end foreach j
-} // end gauss_jordan_elimination()
+} // end gaussJordanElimination()
+
+unittest {
+    Matrix Ab = new Matrix([[0.0,  2.0,  0.0,  1.0,  0.0],
+			    [2.0,  2.0,  3.0,  2.0, -2.0],
+			    [4.0, -3.0,  0.0,  1.0, -7.0],
+			    [6.0,  1.0, -6.0, -5.0,  6.0]]);
+    Matrix Aonly = Ab.sliceDup(0, 4, 0, 4);
+    Matrix bonly = Ab.sliceDup(0, 4, 4, 5);
+    gaussJordanElimination(Ab);
+    assert(approxEqualMatrix(Ab, new Matrix([[1,0,0,0,-0.5],[0,1.0,0,0,1],
+					     [0,0,1,0,1.0/3],[0,0,0,1,-2.0]])),
+	   "Gauss-Jordan elimination with partial pivoting");
+    double[] x = Ab.getColumn(4);
+    Matrix new_rhs = dot(Aonly, new Matrix(x));
+    assert(approxEqualMatrix(new_rhs, bonly), "check rhs");
+    Matrix residual = new_rhs - bonly;
+    assert(approxEqualMatrix(residual, new Matrix([0,0,0,0])), "zero residual");
+}
--- dlang/nm/bbla_demo.d
+++ dlang/nm/bbla_demo.d
@@ -11,14 +11,18 @@ void main()
     Matrix b = zeros(2,3);
     Matrix c = transpose(b);
     b[1,2] = 99.0;
-    c[0,0] = 1.0;
+    c[0,0] = 1.0; c[1,1] = 1.0;
     writeln("b=", b);
     writeln("c=", c);
 
     Matrix e = new Matrix([1.0, 2.0, 3.0]);
     writeln("e= ", e);
+    Matrix e2 = new Matrix([1, 2, 3], "row");
+    writeln("e2= ", e2);
     Matrix f = new Matrix([[1.0,2.0,3.0],[4.0,5.0,6.0]]);
     writeln("f= ", f);
+    writeln("2*f= ", 2*f);
+    writeln("f+2= ", f+2);
 
     Matrix g = dot(f,c);
     writeln("g= ", g);
@@ -27,7 +31,15 @@ void main()
 			    [2.0,  2.0,  3.0,  2.0, -2.0],
 			    [4.0, -3.0,  0.0,  1.0, -7.0],
 			    [6.0,  1.0, -6.0, -5.0,  6.0]]);
-    gauss_jordan_elimination(Ab);
+    Matrix Aonly = Ab.sliceDup(0, 4, 0, 4);
+    Matrix bonly = Ab.sliceDup(0, 4, 4, 5);
+    gaussJordanElimination(Ab);
     writeln("Ab= ", Ab);
+    double[] x = Ab.getColumn(4);
+    writeln("x= ", x);
+    Matrix rhs = dot(Aonly, new Matrix(x));
+    writeln("rhs= ", rhs);
+    Matrix residual = rhs - bonly;
+    writeln("residual= ", residual);
     writeln("Done.");
 }
