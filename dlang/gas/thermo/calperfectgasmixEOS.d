3dee5f2b8006f6ee3ab64a405b97d505fcf6f333
Rowan J. Gollan <r.gollan@uq.edu.au>
2014-09-07 17:46:31 +1000

    dlang/gas: Added mixture models for ideal gases.
    The thermal and caloric equations of state for a mixture
    of ideal gases have been added. Before I can build the complete
    ideal gas mixture model, I need to bring over the mixture
    rules for viscosity and thermal conductivity.

---
+++ dlang/gas/thermo/calperfectgasmixEOS.d
@@ -0,0 +1,74 @@
+/**
+ * calperfectgasEOS.d
+ * Implements a calorically perfect gas mixture equation
+ * of state. In this model, the specific heats
+ * at constant pressure (Cp) and constant volume (Cv)
+ * are functions of the gas composition. The specific heats
+ * for each gas component are constant for all temperatures.
+ *
+ * Author: Rowan G. and Peter J.
+ * Version: 2014-09-07 -- first cut
+ */
+
+import gasmodel;
+import caloricEOS;
+import calperfectgasEOS;
+
+/++
+  CaloricallyPerfectGasMixEOS is a caloric equation of state.
+
+  This model assumes that Cp and Cv are constant with temperature
+  for all gas components.
++/
+class CaloricallyPerfectGasMixEOS : CaloricEOS {
+public:
+    this(double[] Cv, double[] e0) {
+	_Cv = Cv.dup;
+	_e0 = e0.dup;
+    }
+
+    /++
+      Compute the internal energy assuming that temperature
+      is up-to-date in GasState Q.
+    +/
+    override void update_energy(ref GasState Q) const {
+	assert(Q.T.length == 1);
+	assert(Q.e.length == 1);
+	double Cv = mass_average(Q, _Cv);
+	double e0 = mass_average(Q, _e0);
+	Q.e[0] = energy(Q.T[0], Cv, e0);
+    }
+
+    /++ Compute the temperature assuming that the internal energy
+     is up-to-date in GasState Q.
+    +/
+    override void update_temperature(ref GasState Q) const {
+	assert(Q.T.length == 1);
+	assert(Q.e.length == 1);
+	double Cv = mass_average(Q, _Cv);
+	double e0 = mass_average(Q, _e0);
+	Q.T[0] = temperature(Q.e[0], Cv, e0);
+    }
+
+private:
+    double[] _Cv; /// specific heats at constant volume in J/(kg.K)
+    double[] _e0; /// reference energies value in J/kg
+}
+
+unittest {
+    import std.math;
+    double[] Cv = [743.0, 659.0];
+    double[] e0 = [0.0, 0.0];
+    auto cpg = new CaloricallyPerfectGasMixEOS(Cv, e0);
+    auto gd = GasState(2, 1);
+    gd.T[0] = 500.0;
+    gd.e[0] = 0.0;
+    gd.massf[0] = 0.78;
+    gd.massf[1] = 0.22;
+    cpg.update_energy(gd);
+    assert(approxEqual(gd.e[0], 362260.0));
+    gd.e[0] = 362260.0;
+    gd.T[0] = 0.0;
+    cpg.update_temperature(gd);
+    assert(approxEqual(gd.T[0], 500.0));
+}
---
+++ dlang/gas/thermo/perfectgasmixEOS.d
@@ -0,0 +1,83 @@
+/**
+ * perfectgasmixEOS.d
+ * Implements a mixture of perfect gases equation of state.
+ * This module provides simple functions for the
+ * the p-v-T behaviour of a mixture perfect gases.
+ * 
+ * Author: Rowan G. and Peter J.
+ * Version: 2014-09-07 -- first cut
+ */
+
+import gasmodel;
+import thermalEOS;
+import perfectgasEOS;
+
+/++
+ PerfectGasMixEOS is a thermal equation of state.
+ 
+ The perfect gas mixture model assumes point masses and
+ perfectly elastic collisions.
++/
+class PerfectGasMixEOS : ThermalEOS {
+public:
+    this(in double[] R) {
+	_R = R.dup;
+    }
+
+    /++
+      Compute the pressure assuming density and temperature
+      are up-to-date in GasState Q.
+    +/
+    override void update_pressure(ref GasState Q) const {
+	double Rmix = mass_average(Q, _R);
+	Q.p = pressure(Q.rho, Q.T[0], Rmix);
+    }
+
+    /++
+      Compute the density assuming pressure and temperature
+      are up-to-date in GasState Q.
+    +/
+    override void update_density(ref GasState Q) const {
+	double Rmix = mass_average(Q, _R);
+	Q.rho = density(Q.p, Q.T[0], Rmix);
+    }
+
+    /++
+      Compute the temperature assuming density and pressure
+      are up-to-date in GasState Q.
+    +/
+    override void update_temperature(ref GasState Q) const {
+	double Rmix = mass_average(Q, _R);
+	Q.T[0] = temperature(Q.rho, Q.p, Rmix);
+    }
+
+private:
+    double[] _R; /// specific gas constants in J/(kg.K)
+}	  
+
+unittest {
+    import std.math;
+    double[] R = [297.0, 260.0]; // N2, O2
+    auto pg = new PerfectGasMixEOS(R);
+    auto gd = GasState(2, 1);
+    gd.T[0] = 300.0;
+    gd.rho = 1.2;
+    gd.massf[0] = 0.78;
+    gd.massf[1] = 0.22;
+    pg.update_pressure(gd);
+    assert(approxEqual(gd.p, 1103989.6));
+    gd.p = 103989.6;
+    gd.rho = 0.0;
+    pg.update_density(gd);
+    assert(approxEqual(gd.rho, 1.2));
+    gd.rho = 1.2;
+    gd.T[0] = 0.0;
+    pg.update_temperature(gd);
+    assert(approxEqual(gd.T[0], 300.0));
+}
+
+
+
+	   
+
+
