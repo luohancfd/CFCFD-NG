8e041cb05f35a674292984a2019b47e4a4827c1b
Peter Jacobs <peterj@mech.uq.edu.au>
2015-04-22 17:17:55 +1000

    Eilmer4 n90-cylinder case: build our own grid, as per Eilmer3 example.

--- examples/eilmer4/2D/n90-cylinder/n90.lua
+++ examples/eilmer4/2D/n90-cylinder/n90.lua
@@ -1,5 +1,6 @@
 -- n90.lua -- Cylinder in dissociating nitrogen flow
 -- Rg & PJ  2015-03-09
+--          2015-04-22 build an original grid in this script
 
 job_title = "Cylinder in dissociating nitrogen flow."
 print(job_title)
@@ -13,15 +14,36 @@ inflow = FlowState:new{p=500.0, T=700.0, u=5000.0, massf={1.0, 0.0}}
 print "setting initial"
 initial = FlowState:new{p=5.0, T=300.0, massf={1.0, 0.0}}
 
+print "Building grid."
+a = Vector3:new{0.045,    0.0}
+b = Vector3:new{0.0,      0.0}
+c = Vector3:new{0.013181, 0.031820}
+d = Vector3:new{0.045,    0.045}
+e = Vector3:new{0.0675,   0.038972}
+f = Vector3:new{-0.020,    0.0}
+g = Vector3:new{-0.020,    0.050625}
+h = Vector3:new{-0.016875, 0.106875}
+i = Vector3:new{ 0.045,    0.135}
+j = Vector3:new{ 0.07875,  0.095625}
+k = Vector3:new{ 0.084375, 0.0675}
 
-print "Reading Gridpro grid."
+bc = Arc:new{b, c, a}
+cd = Arc:new{c, d, a}
+de = Arc:new{d, e, a}
+e_path = Polyline:new{bc, cd, de}
+w_path = Bezier:new{f, g, h, i}
+s_path = Line:new{f, b}
+n_path = Bezier:new{i, j, k, e}
 
+psurf = makePatch{n_path, e_path, s_path, w_path}
+grid = StructuredGrid:new{psurface=psurf, niv=61, njv=41}
+blk0 = SBlock:new{grid=grid, fillCondition=inflow, label="blk-0"}
 
 -- Grid has been built earlier in GridPro
-gproName = 'n90.gpro'
-grid = importGridproGrid(gproName)
+-- gproName = 'n90.gpro'
+-- grid = importGridproGrid(gproName)
+-- blk0 = SBlock:new{grid=grid[0], fillCondition=inflow, label="blk-0"}
 
-blk0 = SBlock:new{grid=grid[1], fillCondition=inflow, label="blk-0"}
 -- We can leave east and south as SlipWalls
 blk0.bcList[west] = SupInBC:new{flowCondition=inflow}
 blk0.bcList[north] = ExtrapolateOutBC:new{}
