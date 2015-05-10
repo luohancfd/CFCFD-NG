b263499acbbb403fb0fc5fcc8f9e9ed24bb3cfc9
Peter Jacobs <peterj@mech.uq.edu.au>
2015-05-11 08:45:58 +1000

    Eilmer4: inviscid MMS case running with 4 blocks on one CPU.
    Have yet to give the user-defined functions their own thread-local workspace
    and configuration memory.

--- examples/eilmer4/2D/manufactured-solution/mms.lua
+++ examples/eilmer4/2D/manufactured-solution/mms.lua
@@ -5,36 +5,29 @@
 -- History: Ported from eilmer3 example
 --          Presently only Euler case on regular grid
 
-title = "Method of Manufactured Solutions, Euler case."
-print(title)
+config.title = "Method of Manufactured Solutions, Euler case."
+print(config.title)
 config.dimensions = 2
-config.title = title
 
 setGasModel('ideal-air-gas-model.lua')
-
-p0 = 1.0e5
-T0 = p0/287.10325
-u0 = 800.0
-v0 = 800.0
-
+p0 = 1.0e5; T0 = p0/287.10325; u0 = 800.0; v0 = 800.0
 initial = FlowState:new{p=p0, T=T0, velx=u0, vely=v0}
 
-a = Vector3:new{0.0, 0.0}
-b = Vector3:new{1.0, 0.0}
-c = Vector3:new{0.0, 1.0}
-d = Vector3:new{1.0, 1.0}
+p00 = Vector3:new{0.0, 0.0}
+p10 = Vector3:new{1.0, 0.0}
+p01 = Vector3:new{0.0, 1.0}
+p11 = Vector3:new{1.0, 1.0}
+nicell = 16; njcell = 16
+grid = StructuredGrid:new{psurface=CoonsPatch:new{p00=p00, p10=p10, p11=p11, p01=p01},
+			  niv=nicell+1, njv=njcell+1}
 
-nx0 = 16
-ny0 = 16
-
-grid = StructuredGrid:new{psurface=makePatch{Line:new{c,d}, Line:new{b,d}, Line:new{a,b}, Line:new{a,c}},
-			  niv=nx0+1, njv=ny0+1}
-
-blk = SBlock:new{grid=grid, fillCondition=initial, label="blk"}
-blk.bcList[north] = ExtrapolateOutBC:new{xOrder=1}
-blk.bcList[east] = ExtrapolateOutBC:new{xOrder=1}
-blk.bcList[south] = UserDefinedBC:new{fileName='udf-bc.lua'}
-blk.bcList[west] = UserDefinedBC:new{fileName='udf-bc.lua'}
+bcList = {}
+bcList[north] = ExtrapolateOutBC:new{xOrder=1}
+bcList[east] = ExtrapolateOutBC:new{xOrder=1}
+bcList[south] = UserDefinedBC:new{fileName='udf-bc.lua'}
+bcList[west] = UserDefinedBC:new{fileName='udf-bc.lua'}
+blks = SBlockArray{grid=grid, fillCondition=initial, bcList=bcList, 
+		   nib=2, njb=2, label="blk"}
 
 config.interpolation_order = 2
 config.gasdynamic_update_scheme = "predictor-corrector"
