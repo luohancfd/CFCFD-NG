ea5af85aa1a0f56b66e7d92b386322930a15e27f
Peter Roman <devnull@localhost>
2015-07-06 11:22:18 +1000

    dlang/geom: add derivatives for bezier curve

--- dlang/geom/gpath.d
+++ dlang/geom/gpath.d
@@ -280,7 +280,7 @@ class Arc3 : Arc {
 	auto s_values = lsqsolve(amatrix, rhs);
 	double s_am = s_values[0,0];
 	double s_mb = s_values[1,0];
-        Vector3 c = mid_am + s_am * bisect_am;
+	Vector3 c = mid_am + s_am * bisect_am;
 	Vector3 c_check = mid_mb + s_mb * bisect_mb;
 	if (abs(c_check - this.c) > 1.0e-9) {
 	    throw new Error(text("Arc3: Points inconsistent centre estimates.",
@@ -320,7 +320,11 @@ public:
 	if (B.length == 0) {
 	    throw new Error(text("Bezier() No control points present."));
 	}
+	if (B.length == 1) {
+	    throw new Error(text("Bezier() Only one control point, not enough for a curve."));
+	}
 	this.B = B.dup();
+	set_deriv_control_points();
     }
     this(ref const(Bezier) other)
     {
@@ -333,6 +337,46 @@ public:
     override Vector3 opCall(double t) const 
     {
 	// Evaluate B(t)
+	return deCasteljau(B, t);
+    } // end opCall()
+    override Vector3 dpdt(double t) const
+    {
+	return deCasteljau(C, t);
+    }
+    override Vector3 d2pdt2(double t) const
+    {
+	return deCasteljau(D, t);
+    }
+    override string toString() const
+    {
+	return "Bezier(B=" ~ to!string(B) ~ ")";
+    }
+
+protected:
+    Vector3[] C; // derivative curve
+    Vector3[] D; // second derivative curve
+    void set_deriv_control_points()
+    {
+	size_t n = B.length - 1;
+	if ( n == 0) {
+	    // shouldn't reach here due to check in constructor
+	    throw new Error(text("Bezier() Curve is a point, derivative not defined"));
+	}
+	C.length = n;
+	foreach (i; 0 .. n){
+	    C[i] = n*(B[i+1] - B[i]);
+	}
+	if ( n == 1 ) {
+	    D = [Vector3(0)];
+	    return;
+	}
+	D.length = n - 1;
+	foreach (i; 0 .. n-1){
+	    D[i] = (n-1)*(C[i+1] - C[i]);
+	}
+    }
+    Vector3 deCasteljau(ref const(Vector3[]) B, double t) const
+    {
 	if ( B.length == 1 ) return B[0];
 	size_t n_order = B.length - 1;
 	// Apply de Casteljau's algorithm. 
@@ -343,10 +387,6 @@ public:
 	    }
 	}
 	return Q[0];
-    } // end opCall()
-    override string toString() const
-    {
-	return "Bezier(B=" ~ to!string(B) ~ ")";
     }
 } // end class Bezier
 
@@ -360,6 +400,12 @@ unittest {
     auto adb = new Bezier([a, d, b]);
     auto e = adb(0.5);
     assert(approxEqualVectors(e, Vector3(1.60355, 2, 0.603553)), "Bezier");
+    auto ab = new Bezier([a, b]);
+    assert(approxEqualVectors(ab.dpdt(0.5), Vector3(-1, 0, 1)), "Bezier");
+    assert(approxEqualVectors(ab.d2pdt2(0.5), Vector3(0)), "Bezier");
+    auto acb = new Bezier([a, c, b]);
+    assert(approxEqualVectors(acb.dpdt(0.5), Vector3(-1, 0, 1)), "Bezier");
+    assert(approxEqualVectors(acb.d2pdt2(0.5), Vector3(2,0,2)), "Bezier");
 }
 
 
