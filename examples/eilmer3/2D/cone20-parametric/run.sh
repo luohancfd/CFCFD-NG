026050ea151d7698308135e3dd202878c0154fe6
Peter Jacobs <peterj@mech.uq.edu.au>
2015-07-25 14:30:44 +1000

    Eilmer3 user-guide: made a more interesting parametric example.

---
+++ examples/eilmer3/2D/cone20-parametric/run.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+# run.sh
+# exercise the Navier-Stokes solver for the cone20-parametric test case.
+# It is assumed that the path is set correctly.
+
+# Prepare the simulation input files (parameter, grid and initial flow data).
+# The SVG file provides us with a graphical check on the geometry
+e3prep.py --job=conep --do-svg
+if [ "$?" -ne "0" ] ; then
+    echo "e3prep.py ended abnormally."
+    exit
+fi
+
+# Integrate the solution in time, 
+# recording the axial force on the cone surface.
+time e3shared.exe -f conep --run --verbose
+if [ "$?" -ne "0" ] ; then
+    echo "e3shared.exe ended abnormally."
+    exit
+fi
+
+# Extract the solution data and reformat.
+# If no time is specified, the final solution found is output.
+e3post.py --job=conep --vtk-xml
+
+echo "At this point, we should have a solution that can be viewed with Paraview."
