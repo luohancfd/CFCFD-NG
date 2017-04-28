8b2ece02b561ef834d70cb4407323b920a5bf761
Rowan J. Gollan <r.gollan@uq.edu.au>
2017-04-28 06:05:42 -0400

    Eilmer3/examples: Added Sod shock tube example with TransientProfileBC in action.

---
+++ examples/eilmer3/2D/sod/N2-O2-w-trans-prof-bc/hist2transprof.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Author: Rowan J. Gollan
+# Date: 2017-04-28
+#
+# This script is used to convert a file with two adjacent history cells
+# into a transient profile boundary condition data over a slice of cells.
+
+ncellsInProfile = 2
+inpFile = "hist/sod.hist.b0000"
+outFile = "sod-driver-transient-profile-bc.dat"
+
+ofp = open(outFile, "w")
+ofp.write("# data converted from hist file.\n")
+ofp.write("# Variables list: x y z volume rho u v w p a mu k0 mu_t k_t S tke omega mf0 mf1 dt_chem e0 T0\n")
+ofp.write("# 2\n")
+
+ifp = open(inpFile, "r")
+ifp.readline() # Throw away header line
+
+# Now begin looping through file.
+# We'll work in pairs of lines and then expand that 
+# out for the full profile in the out file.
+
+while True:
+    rghtCell = ifp.readline()
+    if not rghtCell:
+        break
+    rTks = rghtCell.split()
+    leftCell = ifp.readline()
+    lTks = leftCell.split()
+    t = rTks[0]
+    ofp.write("%s\n" % t)
+    for i in range(ncellsInProfile):
+        ofp.write("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" %
+                  (rTks[4], rTks[5], rTks[6], rTks[7], rTks[8], rTks[9], rTks[10], rTks[11],
+                   rTks[12], rTks[13], rTks[14], rTks[15], rTks[16], rTks[17], rTks[18], rTks[19], rTks[20],
+                   rTks[21], rTks[22], rTks[23], rTks[24], rTks[25]))
+        ofp.write("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" %
+                  (lTks[4], lTks[5], lTks[6], lTks[7], lTks[8], lTks[9], lTks[10], lTks[11],
+                   lTks[12], lTks[13], lTks[14], lTks[15], lTks[16], lTks[17], lTks[18], lTks[19], lTks[20],
+                   lTks[21], lTks[22], lTks[23], lTks[24], lTks[25]))
+
+ifp.close()
+ofp.close()
+                   
+        
+        
+
+
+
+
+
---
+++ examples/eilmer3/2D/sod/N2-O2-w-trans-prof-bc/sod.py
@@ -0,0 +1,59 @@
+# Sod's shock tube problem with a mixture of ideal gases
+#
+# Description
+# -----------
+# A mixture of two species N2 and O2 with mass fractions: 0.7778 and 0.2222
+# respectively is present. This is roughly 80% N2 and 20% O2 by volume which is
+# representative of room temperature air.
+#
+# This test case has been adapted from an old mb_cns test case:
+#  sod_mix/test_case_1.sit
+#
+# Author: Rowan J. Gollan
+# Date: 30-July-2008
+
+gdata.title = "One-dimensional shock tube with (fake) air driving (fake) air."
+gdata.dimensions = 2
+gdata.stringent_cfl = 1
+gdata.gasdynamic_update_scheme = "classic-rk3"
+gdata.cfl = 1.10
+
+select_gas_model(model='ideal gas',
+                 species=['N2', 'O2'])
+
+high_p = FlowCondition(p=1.0e5, u=0.0, v=0.0, T=348.4, massf=[0.7778, 0.2222])
+low_p = FlowCondition(p=1.0e4, u=0.0, v=0.0, T=278.7, massf=[0.7778, 0.2222])
+
+# Set up two quadrilaterals in the (x,y)-plane by first defining
+# the corner nodes, then the lines between those corners.
+# The labelling is not significant; it is just to make the MetaPost
+# picture look the same as that produced by the Tcl scriptit program.
+a = Node(0.5, 0.0, label="a")
+b = Node(0.5, 0.1, label="b")
+c = Node(0.25, 0.1, label="c")
+d = Node(0.25, 0.0,  label="d")
+e = Node(1.0, 0.0, label="e")
+f = Node(1.0, 0.1, label="f")
+
+south0 = Line(d, a); south1 = Line(a, e) # lower boundary along x-axis
+north0 = Line(c, b); north1 = Line(b, f) # upper boundary
+west0 = Line(d, c); east0west1 = Line(a, b); east1 = Line(e, f)
+
+# Define the blocks, boundary conditions and set the discretisation.
+nx = 50; ny = 2
+blk_0 = Block2D(make_patch(north0, east0west1, south0, west0), nni=nx/2, nnj=ny,
+                fill_condition=high_p, label="driver")
+blk_1 = Block2D(make_patch(north1, east1, south1, east0west1), nni=nx, nnj=ny,
+                fill_condition=low_p, label="driven")
+identify_block_connections()
+blk_0.bc_list[WEST] = TransientProfBC(filename="sod-driver-transient-profile-bc.dat", n_profile=2)
+
+# Some simulation parameters
+gdata.flux_calc = AUSMDV
+gdata.max_time = 0.6e-3
+gdata.max_step = 600
+gdata.dt = 1.0e-6
+gdata.dt_history = 1.0e-5
+
+
+
